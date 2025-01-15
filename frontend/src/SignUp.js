be710721179b613d76c8ac2c0e5c3a7e0bdde708
Siddhant22497 <133579862+Siddhant22497@users.noreply.github.com>
2025-01-15 18:19:15 +0530

    Add files via upload

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -83,7 +83,7 @@ function SignUp() {
       }
     })
     setTimeout(() => {
-      navigate('/Homescreen', { state: { username: username, password: password } });
+      navigate('/Homescreen', { state: { "username": username } });
     }, 1000);
 
   }
